cc668438801dfb8d86cc7d0f0583e271bec1569f
zengbailiang <634825162@qq.com>
2018-05-12 10:13:48 +0800

    Initial Commit

---
+++ TouchIdDeamSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIdDeamSwift
+//
+//  Created by zengbailiang on 2018/5/12.
+//  Copyright © 2018年 zengbailiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
